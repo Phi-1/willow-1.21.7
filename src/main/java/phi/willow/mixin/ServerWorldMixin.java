99ea8376769e58f924dd857c3c3959050371f79d
Phi <dterberg98@gmail.com>
2025-08-25 10:06:49 +0200

    players wake up just before sunrise

--- src/main/java/phi/willow/mixin/ServerWorldMixin.java
+++ src/main/java/phi/willow/mixin/ServerWorldMixin.java
@@ -3,6 +3,7 @@ package phi.willow.mixin;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -17,6 +18,12 @@ public abstract class ServerWorldMixin {
         if (ThreadLocalRandom.current().nextInt(3) == 0)
             return;
         instance.resetWeather();
+    }
+
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V"))
+    public void wakeUpEarlier(ServerWorld instance, long timeOfDay)
+    {
+        instance.setTimeOfDay(timeOfDay - 1500);
     }
 
 }
